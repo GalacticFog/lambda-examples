dc4bb794a2eb4a2122b23b35b08a9a6273168d79
Christopher Baker <cgbaker@gmail.com>
2017-02-18 14:00:35 -0500

    made populate_demo async

--- js_lambda/demo-env.js
+++ js_lambda/demo-env.js
@@ -1,17 +1,15 @@
-
-var META = null;
-
-var LOG = new java.lang.StringBuilder();
-
-var AsyncHttpClient = Java.type('com.ning.http.client.AsyncHttpClient');
-var client = new AsyncHttpClient();
+var AsyncHttpClient   = Java.type('com.ning.http.client.AsyncHttpClient');
 var CompletableFuture = Java.type('java.util.concurrent.CompletableFuture');
 
-function setup_demo(args, cred) {
+var META   = null;
+var LOG    = new java.lang.StringBuilder();
+var client = new AsyncHttpClient();
+
+function setup_demo(/* arguments, credentials */) {
     META = get_meta();
     log("found meta: " + JSON.stringify(META));
 
-    var root_org = GET("/root");
+    var root_org = find_org("/root");
     log("found root org:" + root_org.id);
 
     // find kong provider
@@ -22,7 +20,7 @@ function setup_demo(args, cred) {
 
     var trading_grp = create_group(root_org, "trading", "Trading group");
     var joe_the_trader = create_user(root_org, {
-        description: "Principal Engineer/Trading group",
+        description: "Principal Engineer (Trading group)",
         name: "jdoe",
         properties: {
             firstName: "J",
@@ -43,7 +41,7 @@ function setup_demo(args, cred) {
     }
 
     // create marathon-dev provider in new org
-    var dev_provider = create_provider(demo_org, {
+    create_provider(demo_org, {
             name: "marathon-dev",
             resource_type: "Gestalt::Configuration::Provider::Marathon",
             description: "",
@@ -60,25 +58,26 @@ function setup_demo(args, cred) {
     add_entitlement(root_org, root_org, "provider.view", trading_grp, true);
 
     // create and populate demo sub-orgs: hr, it, debt, equity, private-client
-    var hr_demo     = populate_demo_org("hr",             "HR Division", demo_org);
-    var it_demo     = populate_demo_org("it",             "IT Division", demo_org);
-    var debt_demo   = populate_demo_org("debt",           "Debt Division", demo_org);
-    var equity_demo = populate_demo_org("equity",         "Equity Division", demo_org);
-    var pc_demo     = populate_demo_org("private-client", "Private Client Division", demo_org);
+    var fEquityDiv = populate_demo_org("equity",         "Equity Division",         demo_org);
+    var fHrDiv     = populate_demo_org("hr",             "HR Division",             demo_org);
+    var fItDiv     = populate_demo_org("it",             "IT Division",             demo_org);
+    var fDebtDiv   = populate_demo_org("debt",           "Debt Division",           demo_org);
+    var fPCDiv     = populate_demo_org("private-client", "Private Client Division", demo_org);
 
     // additionally, equity.galactic-capital gets workspace "trading"
-    var trading_wrk  = create_workspace(equity_demo.org, "trading", "Trading application platform");
-    var trading_dev  = create_environment(equity_demo.org, trading_wrk, "dev", "Development", EnvironmentTypes.DEVELOPMENT);
-    var trading_prod = create_environment(equity_demo.org, trading_wrk, "prod", "Production", EnvironmentTypes.PRODUCTION);
-    var trading_qa   = create_environment(equity_demo.org, trading_wrk, "qa",   "QA",         EnvironmentTypes.TEST);
+    var equityDiv = fEquityDiv.get();
+    var trading_wrk  = create_workspace(equityDiv, "trading", "Trading application platform");
+    var trading_dev  = create_environment(equityDiv, trading_wrk, "dev", "Development", EnvironmentTypes.DEVELOPMENT);
+    var trading_prod = create_environment(equityDiv, trading_wrk, "prod", "Production", EnvironmentTypes.PRODUCTION);
+    var trading_qa   = create_environment(equityDiv, trading_wrk, "qa",   "QA",         EnvironmentTypes.TEST);
 
-    add_entitlement(demo_org,        demo_org,     "org.view",         trading_grp, true);
-    add_entitlement(equity_demo.org, trading_wrk,  "workspace.view",   trading_grp, true);
-    add_entitlement(equity_demo.org, trading_wrk,  "environment.view", trading_grp, true);
+    add_entitlement(demo_org,  demo_org,     "org.view",         trading_grp, true);
+    add_entitlement(equityDiv, trading_wrk,  "workspace.view",   trading_grp, true);
+    add_entitlement(equityDiv, trading_wrk,  "environment.view", trading_grp, true);
     for each (env in [trading_dev, trading_prod, trading_qa]) {
         for each (ent in ["container.create", "container.view", "container.update", "container.delete", "container.scale", "container.migrate",
                           "lambda.create", "lambda.view", "lambda.update", "lambda.delete"]) {
-            add_entitlement(equity_demo.org, env, ent, trading_grp, true);
+            add_entitlement(equityDiv, env, ent, trading_grp, true);
         }
     }
 
@@ -113,9 +112,9 @@ function setup_demo(args, cred) {
         }
     });
     log("created migrate lambda: " + migrate_lambda.id);
-    create_migrate_policy(equity_demo.org, equity_demo.dev,  migrate_lambda);
-    create_migrate_policy(equity_demo.org, equity_demo.prod, migrate_lambda);
-    create_migrate_policy(equity_demo.org, equity_demo.qa,   migrate_lambda);
+    create_migrate_policy(fEquityDiv.org, fEquityDiv.dev,  migrate_lambda);
+    create_migrate_policy(fEquityDiv.org, fEquityDiv.prod, migrate_lambda);
+    create_migrate_policy(fEquityDiv.org, fEquityDiv.qa,   migrate_lambda);
 
     log("\nDemo environment complete.")
     return LOG.toString();
@@ -133,19 +132,17 @@ var ProviderTypes = {
 };
 
 function populate_demo_org(name, description, base_org) {
-    var new_org = create_org(base_org, name, description);
-    var new_wrk = create_workspace(new_org, name+"-platform", description + " application platform");
-    var dev     = create_environment(new_org, new_wrk, "dev",  "Development", EnvironmentTypes.DEVELOPMENT);
-    var prod    = create_environment(new_org, new_wrk, "prod", "Production",  EnvironmentTypes.PRODUCTION);
-    var qa      = create_environment(new_org, new_wrk, "qa",   "QA",          EnvironmentTypes.TEST);
-
-    return {
-        org: new_org,
-        workspace: new_wrk,
-        dev: dev,
-        prod: prod,
-        qa: qa
-    }
+    // asynchronously create org, workspace, and three environments
+    var new_org = create_org(base_org, name, description, true);
+    new_org.thenApply(function (org) {
+        var new_wrk = create_workspace(new_org, name+"-platform", description + " application platform", true);
+        new_wrk.thenApply(function (wrk) {
+            create_environment(org, wrk, "dev",  "Development", EnvironmentTypes.DEVELOPMENT, true);
+            create_environment(org, wrk, "prod", "Production",  EnvironmentTypes.PRODUCTION,  true);
+            create_environment(org, wrk, "qa",   "QA",          EnvironmentTypes.TEST,        true);
+        })
+    });
+    return new_org;
 }
 
 function get_env(key) {
@@ -179,7 +176,7 @@ function log(a) {
 function list_providers(org, provider_type) {
     var endpoint = "/" + org.properties.fqon + "/providers?expand=true";
     if (provider_type) endpoint = endpoint + "&type=" + provider_type;
-    return GET(endpoint);
+    return _GET(endpoint);
 }
 
 function fqon(org) {
@@ -188,14 +185,14 @@ function fqon(org) {
 
 function create_group(parent_org, name, desc) {
     log("Creating group " + parent_org.name + "/" + name);
-    return POST("/" + fqon(parent_org) + "/groups", {
+    return _POST("/" + fqon(parent_org) + "/groups", {
         name: name,
         description: desc
     });
 }
 
 function find_entitlement_async(base_org, resource, entitlement_name) {
-    var fEnts = GET("/" + fqon(base_org) + "/resources/" + resource.id + "/entitlements?expand=true", true);
+    var fEnts = _GET("/" + fqon(base_org) + "/resources/" + resource.id + "/entitlements?expand=true", true);
     return fEnts.thenApply(function(ents) {
         for each (e in ents) if (e.properties.action == entitlement_name) return e;
         return null;
@@ -234,13 +231,13 @@ function add_entitlement(base_org, resource, entitlement_name, identity, async) 
         log((async ? "[async]" : "") + "updating entitlement " + resource.name + "[" + entitlement_name + "] with " + disp(identity));
         switch (resource.resource_type) {
             case "Gestalt::Resource::Environment":
-                return PUT("/" + fqon(base_org) + "/environments/" + resource.id + "/entitlements/" + ent.id, new_ent, true);
+                return _PUT("/" + fqon(base_org) + "/environments/" + resource.id + "/entitlements/" + ent.id, new_ent, true);
             case "Gestalt::Resource::Workspace":
-                return PUT("/" + fqon(base_org) + "/workspaces/" + resource.id + "/entitlements/" + ent.id, new_ent, true);
+                return _PUT("/" + fqon(base_org) + "/workspaces/" + resource.id + "/entitlements/" + ent.id, new_ent, true);
             case "Gestalt::Resource::Organization":
-                return PUT("/" + fqon(resource) + "/entitlements/" + ent.id, new_ent, true);
+                return _PUT("/" + fqon(resource) + "/entitlements/" + ent.id, new_ent, true);
             default:
-                return PUT("/" + fqon(base_org) + "/entitlements/" + ent.id, new_ent, true);
+                return _PUT("/" + fqon(base_org) + "/entitlements/" + ent.id, new_ent, true);
         }
     });
     var _async = async ? async : false; // just being explicit that the default here is 'false'
@@ -250,14 +247,14 @@ function add_entitlement(base_org, resource, entitlement_name, identity, async) 
 
 function create_migrate_policy(base_org, environment, lambda) {
     log("creating new migrate policy in " + environment.name);
-    var pol = POST("/" + fqon(base_org) + "/environments/" + environment.id + "/policies", {
+    var pol = _POST("/" + fqon(base_org) + "/environments/" + environment.id + "/policies", {
         name: "default-migrate-policy",
         description: "default container migration policy",
         properties: {}
     });
 
     log("creating migrate event rule in migrate policy " + pol.id + " against lambda " + lambda.id);
-    POST("/" + fqon(base_org) + "/policies/" + pol.id + "/rules", {
+    _POST("/" + fqon(base_org) + "/policies/" + pol.id + "/rules", {
         name: "migration-handler",
         description: "execute migrate lambda on container.migrate.pre",
         resource_type: "event",
@@ -271,43 +268,47 @@ function create_migrate_policy(base_org, environment, lambda) {
 
 function create_user(parent_org, account_payload) {
     log("Creating user " + parent_org.name + "/" + account_payload.name);
-    return POST("/" + fqon(parent_org) + "/users", account_payload);
+    return _POST("/" + fqon(parent_org) + "/users", account_payload);
 }
 
 function add_user_to_group(parent_org, group, user) {
     log("Adding user " + user.name + " to group " + group.name);
-    PATCH("/" + fqon(parent_org) + "/groups/" + group.id + "/users?id=" + user.id);
+    _PATCH("/" + fqon(parent_org) + "/groups/" + group.id + "/users?id=" + user.id);
 }
 
 function create_provider(parent_org, provider_payload) {
     log("Creating provider " + provider_payload.name);
-    return POST("/" + fqon(parent_org) + "/providers", provider_payload);
+    return _POST("/" + fqon(parent_org) + "/providers", provider_payload);
 }
 
-function create_org(parent_org, name, description) {
+function find_org(fqon, async) {
+    return _GET(fqon, async);
+}
+
+function create_org(parent_org, name, description, async) {
     log("Creating org " + parent_org.name + "/" + name);
     var payload = {
         description: description,
         name: name
     };
-    return POST("/" + fqon(parent_org), payload)
+    return _POST("/" + fqon(parent_org), payload, async)
 }
 
-function create_workspace(parent_org, name, description) {
+function create_workspace(parent_org, name, description, async) {
     log("Creating workspace " + parent_org.name + "/" + name);
     var payload = {
         description: description,
         name: name
     };
-    return POST("/" + fqon(parent_org) + "/workspaces", payload)
+    return _POST("/" + fqon(parent_org) + "/workspaces", payload, async)
 }
 
 function create_lambda(parent_org, parent_env, lambda_payload) {
     log("Creating lambda " + parent_env.name + "/" + lambda_payload.name);
-    return POST("/" + parent_org.properties.fqon + "/environments/" + parent_env.id + "/lambdas", lambda_payload);
+    return _POST("/" + parent_org.properties.fqon + "/environments/" + parent_env.id + "/lambdas", lambda_payload);
 }
 
-function create_environment(parent_org, parent_workspace, name, description, type) {
+function create_environment(parent_org, parent_workspace, name, description, type, async) {
     log("Creating environment " + parent_workspace.name + "/" + name);
     var payload = {
         description: description,
@@ -316,13 +317,16 @@ function create_environment(parent_org, parent_workspace, name, description, typ
             environment_type: type
         }
     };
-    return POST("/" + parent_org.properties.fqon + "/workspaces/" + parent_workspace.id + "/environments", payload)
+    return _POST("/" + parent_org.properties.fqon + "/workspaces/" + parent_workspace.id + "/environments", payload, async)
 }
 
 function _handleResponse(response) {
     var code = response.getStatusCode();
     var body = response.getResponseBody();
-    if (code >= 300) {
+    if (code == 404) {
+        log("status code 404, resource not found");
+        return null;
+    } else if (code >= 300) {
         log("WARNING: status code : " + code);
         log("response: " + body);
         throw code;
@@ -331,7 +335,7 @@ function _handleResponse(response) {
     return body;
 }
 
-function _REST_JSON(method, endpoint, payload, async) {
+function _REST_JSON(method, endpoint, payload, async, fResponse) {
     var url = META.url + endpoint;
     var pc = client.prepareConnect(url)
         .setMethod(method)
@@ -342,29 +346,29 @@ function _REST_JSON(method, endpoint, payload, async) {
     }
     var _async = async ? async : false; // just being explicit that the default here is 'false'
     if (_async) {
-        var cf = new CompletableFuture();
+        if (!fResponse) fResponse = new CompletableFuture();
         pc.execute(new com.ning.http.client.AsyncCompletionHandler({
             onCompleted: function(response) {
-                cf.complete(_handleResponse(response));
+                fResponse.complete(_handleResponse(response));
             }
         }));
-        return cf;
+        return fResponse;
     }
     return _handleResponse(pc.execute().get());
 }
 
-function GET(endpoint, async) {
-    return _REST_JSON("GET", endpoint, null, async);
+function _GET(endpoint, async, fResponse) {
+    return _REST_JSON("GET", endpoint, null, async, fResponse);
 }
 
-function POST(endpoint, payload) {
-    return _REST_JSON("POST", endpoint, payload);
+function _POST(endpoint, payload, async, fResponse) {
+    return _REST_JSON("POST", endpoint, payload, async, fResponse);
 }
 
-function PUT(endpoint, payload, async) {
-    return _REST_JSON("PUT", endpoint, payload, async);
+function _PUT(endpoint, payload, async, fResponse) {
+    return _REST_JSON("PUT", endpoint, payload, async, fResponse);
 }
 
-function PATCH(endpoint, payload) {
-    return _REST_JSON("PATCH", endpoint, payload);
+function _PATCH(endpoint, payload, async, fResponse) {
+    return _REST_JSON("PATCH", endpoint, payload, async, fResponse);
 }
