c3b2e4195dc2b497c0031070a6206b10d58f38cf
Christopher Baker <cgbaker@gmail.com>
2017-02-20 16:06:03 -0500

    broke sdk out of teardown/setup

---
+++ js_lambda/demo-teardown.js
@@ -0,0 +1,38 @@
+function demo_teardown(/* arguments, credentials */) {
+
+    load();
+
+    META = get_meta();
+    log("[init] found meta: " + META.url);
+    var root_org = find_org("root");
+    log("[init] found root: " + root_org.id);
+    log("")
+
+    var jdoe = find_user(root_org, "jdoe");
+    if (jdoe) {
+        delete_user(root_org, jdoe);
+    } else {
+        log("did not find user 'jdoe'");
+    }
+    log("")
+
+    var trading_grp = find_group(root_org, "trading");
+    if (trading_grp) {
+        delete_group(root_org, trading_grp);
+    } else {
+        log("did not find group 'trading'");
+    }
+    log("")
+
+    var demo_org = find_org("galactic-capital");
+    if (demo_org) {
+        // do this async, because it takes a long time
+        delete_org(demo_org, FORCE_DELETE, DO_ASYNC);
+    } else {
+        log("did not find org 'galactic-capital'");
+    }
+    log("")
+
+    log("Demo environment removed");
+    return LOG.toString();
+}
