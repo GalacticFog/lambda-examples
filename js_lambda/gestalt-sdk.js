140125ef7eeedc5ba3659aa11267c8bf0f955a52
Christopher Baker <cgbaker@gmail.com>
2017-03-02 10:15:39 -0500

    problem with logging level, trying to work through it

--- js_lambda/gestalt-sdk.js
+++ js_lambda/gestalt-sdk.js
@@ -76,9 +76,10 @@ function log(a, lvl) {
     else {
         str = a.toString();
     }
-    if (lvl == undefined) {
+    if (lvl === undefined) {
         lvl = LoggingLevels.INFO;
     }
+    console.log("level is " + lvl);
     if (lvl >= loggingLevels.console) console.log(str);
     if (lvl >= loggingLevels.applog)  LOG_APPENDER.append(str + "\n");
 }
