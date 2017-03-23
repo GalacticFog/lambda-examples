47bd86705020b86c274e8213e2c4994ca4cc53a7
Christopher Baker <cgbaker@gmail.com>
2017-03-23 08:48:51 -0400

    added simple lxml example, haven't tested it yet

---
+++ python_lambda/lxml/test_lxml.py
@@ -0,0 +1,32 @@
+try:
+  from lxml import etree
+  print("running with lxml.etree")
+except ImportError:
+  try:
+    # Python 2.5
+    import xml.etree.cElementTree as etree
+    print("running with cElementTree on Python 2.5+")
+  except ImportError:
+    try:
+      # Python 2.5
+      import xml.etree.ElementTree as etree
+      print("running with ElementTree on Python 2.5+")
+    except ImportError:
+      try:
+        # normal cElementTree install
+        import cElementTree as etree
+        print("running with cElementTree")
+      except ImportError:
+        try:
+          # normal ElementTree install
+          import elementtree.ElementTree as etree
+          print("running with ElementTree")
+        except ImportError:
+          print("Failed to import ElementTree from any known place")
+
+root = etree.Element("root")
+root.append( etree.Element("child1") )
+child2 = etree.SubElement(root, "child2")
+child3 = etree.SubElement(root, "child3")
+
+print(etree.tostring(root, pretty_print=True))
