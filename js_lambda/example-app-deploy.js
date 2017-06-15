3c5631275943ab8db20e081fdf8d1f762e1d07cb
Christopher Baker <cgbaker@gmail.com>
2017-06-14 23:14:20 -0400

    more work on gitlab example

--- js_lambda/example-app-deploy.js
+++ js_lambda/example-app-deploy.js
@@ -5,7 +5,7 @@ function deploy(args, ctx) {
     args = JSON.parse( args );
     ctx  = JSON.parse( ctx );
 
-    META = get_meta(null, null);
+    META = get_meta(args, ctx.creds);
     log("[init] found meta: " + META.url);
 
     var tgt_org = find_org(get_env("TARGET_ORG"));
@@ -32,20 +32,20 @@ function deploy(args, ctx) {
         return getLog();
     }
 
-    var gitlab_url = get_env("GITLAB_API_URL");
+    var gitlab_url   = get_env("GITLAB_API_URL");
     var gitlab_token = get_env("GITLAB_TOKEN");
 
-    var commit_sha = args.substr(0,8);
     var full_sha   = args;
-    var tgt_image  = "galacticfog/example-website:" + commit_sha;
+    var short_sha = full_sha.substr(0,8);
+    var tgt_image  = "galacticfog/example-website:" + short_sha;
 
     log("Will deploy image " + tgt_image + " to provider " + tgt_provider.name);
 
     var new_app;
     try {
         var payload = {
-            name:        "docs-review-" + commit_sha,
-            description: "documentation site deploy by gitlab, commit " + commit_sha,
+            name:        "docs-review-" + short_sha,
+            description: "documentation site deploy by gitlab, commit " + short_sha,
             properties: {
                 provider: {
                     id: tgt_provider.id
@@ -64,7 +64,7 @@ function deploy(args, ctx) {
                     container_port: 80
                 }],
                 labels:         {
-                    "COMMIT_SHA": commit_sha
+                    "COMMIT_SHA": short_sha
                 },
                 force_pull :    true
             }
@@ -77,50 +77,100 @@ function deploy(args, ctx) {
     }
     log("Created new container with id " + new_app.id);
 
-    log("Creating ApiEndpoint target");
     var new_apiendpoint = create_apiendpoint(tgt_org, tgt_api, {
-        name: "docs-review-" + commit_sha,
+        name: "docs-review-" + short_sha,
         properties: {
             implementation_type: "container",
             implementation_id: new_app.id,
             container_port_name: "web",
-            resource: "/docs-review-" + commit_sha + "/"
+            resource: "/docs-review-" + short_sha + "/"
         }
     });
-    var new_url = "https://gtw1.demo7.galacticfog.com/equity-docs" + new_apiendpoint.properties.resource
+    var new_url = "https://gtw1.demo7.galacticfog.com/" + tgt_api.name + new_apiendpoint.properties.resource
     log("Created api-endpoint for container with id " + new_apiendpoint.id + " at " + new_url);
 
-    log("Calling back to GitLab to provision new environment");
-    create_gitlab_environment(gitlab_url, gitlab_token, {
-        id: "galacticfog%2Fexample-website",
-        name: "review-" + commit_sha,
-        external_url: new_url
-    });
+    log("Searching for GitLab Environment");
+    var gitlab_env = find_gitlab_environment(gitlab_url, gitlab_token, "review-" + full_sha);
+    if ( gitlab_env ) {
+        log("Calling back to GitLab to provision new environment");
+        update_gitlab_environment(gitlab_url, gitlab_token, gitlab_env, {
+            external_url: new_url
+        });
+    } else {
+        log("WARNING: Could not locate GitLab environment in order to update external_url");
+    }
 
     log("\n***** done with documentation review deploy ************");
     return getLog();
 }
 
+function stop(args, ctx) {
+    load('https://raw.githubusercontent.com/GalacticFog/lambda-examples/gitlab-demo/js_lambda/gestalt-sdk.js');
+    log("***** begin documentation review stop ************\n");
 
+    args = JSON.parse( args );
+    ctx  = JSON.parse( ctx );
 
-function create_gitlab_environment(base_url, token, payload, async) {
+    META = get_meta(args, ctx.creds);
+    log("[init] found meta: " + META.url);
+
+    var tgt_org = find_org(get_env("TARGET_ORG"));
+    if ( ! tgt_org ) {
+        log("ERROR: could not find target org");
+        return getLog();
+    }
+
+    var tgt_env = find_environment(tgt_org, get_env("TARGET_ENV"));
+    if ( ! tgt_env ) {
+        log("ERROR: could not find target environment");
+        return getLog();
+    }
+
+    var short_sha = args.substr(0,8);
+
+    log("Will delete deployment associated with commit " + short_sha);
+
+    var tgt_cntr = find_container_by_name(tgt_org, tgt_env, "docs-review-" + short_sha);
+
+    if ( tgt_cntr ) {
+        log("Listing endpoints for container");
+        var endpoints = list_container_apiendpoints(tgt_org, tgt_cntr);
+        for each (e in endpoints) {
+            log("deleting endpoint " + e.id);
+            delete_endpoint(tgt_org, e);
+        }
+
+        log("deleting container");
+        delete_container(tgt_org, tgt_env, tgt_cntr);
+    } else {
+        log("did not find any deployments matching that name");
+    }
+
+    log("\n***** done with documentation review stop ************");
+    return getLog();
+}
+
+function find_gitlab_environment(base_url, token, environment_name) {
+    log("search for " + environment_name);
     var url = base_url + "/environments";
     var pc = client.prepareConnect(url)
-        .setMethod("POST")
+        .setMethod("GET")
         .addHeader("PRIVATE-TOKEN", token);
-    log("POST " + url, LoggingLevels.DEBUG);
+    log("GET " + url, LoggingLevels.DEBUG);
+    var envs = _handleResponse(pc.execute().get());
+    for each (e in envs) if (e.name == environment_name) return e;
+    return null;
+}
+
+function update_gitlab_environment(base_url, token, gitlab_env, payload) {
+    var url = base_url + "/environments/" + gitlab_env.id;
+    var pc = client.prepareConnect(url)
+        .setMethod("PUT")
+        .addHeader("PRIVATE-TOKEN", token);
+    log("PUT " + url, LoggingLevels.DEBUG);
     if (payload) {
         pc = pc.setBody(JSON.stringify(payload)).addHeader("Content-Type", "application/json")
     }
-    var _async = async ? async : false; // just being explicit that the default here is 'false'
-    if (_async) {
-        if (!fResponse) fResponse = new CompletableFuture();
-        pc.execute(new org.asynchttpclient.AsyncCompletionHandler({
-            onCompleted: function(response) {
-                fResponse.complete(_handleResponse(response));
-            }
-        }));
-        return fResponse;
-    }
     return _handleResponse(pc.execute().get());
 }
+
