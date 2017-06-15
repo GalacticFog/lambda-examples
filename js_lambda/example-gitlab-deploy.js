ec858c09ea32d53ba5238c3b43e1e47c41c5dc01
Christopher Baker <cgbaker@gmail.com>
2017-06-15 01:32:02 -0400

    finally have the gitlab demo working end-to-end

--- js_lambda/example-gitlab-deploy.js
+++ js_lambda/example-gitlab-deploy.js
@@ -2,10 +2,9 @@ function deploy(args, ctx) {
     load('https://raw.githubusercontent.com/GalacticFog/lambda-examples/gitlab-demo/js_lambda/gestalt-sdk.js');
     log("***** begin documentation review deploy ************\n");
 
-    args = JSON.parse( args );
     ctx  = JSON.parse( ctx );
 
-    META = get_meta(args, ctx.creds);
+    META = get_meta(null, ctx.creds);
     log("[init] found meta: " + META.url);
 
     var tgt_org = find_org(get_env("TARGET_ORG"));
@@ -35,17 +34,20 @@ function deploy(args, ctx) {
     var gitlab_url   = get_env("GITLAB_API_URL");
     var gitlab_token = get_env("GITLAB_TOKEN");
 
-    var full_sha   = args;
+    var full_sha   = ctx.params.sha[0];
     var short_sha = full_sha.substr(0,8);
+    var git_ref    = ctx.params.ref[0];
     var tgt_image  = "galacticfog/example-website:" + short_sha;
 
     log("Will deploy image " + tgt_image + " to provider " + tgt_provider.name);
 
+    var review_app_name = "review-" + git_ref;
+
     var new_app;
     try {
         var payload = {
-            name:        "docs-review-" + short_sha,
-            description: "documentation site deploy by gitlab, commit " + short_sha,
+            name:        "docs-review-" + git_ref,
+            description: "documentation site deploy by gitlab, ref " + git_ref + ", sha " + short_sha,
             properties: {
                 provider: {
                     id: tgt_provider.id
@@ -64,7 +66,9 @@ function deploy(args, ctx) {
                     container_port: 80
                 }],
                 labels:         {
-                    "COMMIT_SHA": short_sha
+                    "COMMIT_SHA": full_sha,
+                    "GIT_REF": git_ref,
+                    "REVIEW_APP": review_app_name
                 },
                 force_pull :    true
             }
@@ -78,19 +82,19 @@ function deploy(args, ctx) {
     log("Created new container with id " + new_app.id);
 
     var new_apiendpoint = create_apiendpoint(tgt_org, tgt_api, {
-        name: "docs-review-" + short_sha,
+        name: "docs-review-" + git_ref,
         properties: {
             implementation_type: "container",
             implementation_id: new_app.id,
             container_port_name: "web",
-            resource: "/docs-review-" + short_sha + "/"
+            resource: "/docs-review-" + git_ref + "/"
         }
     });
     var new_url = "https://gtw1.demo7.galacticfog.com/" + tgt_api.name + new_apiendpoint.properties.resource
     log("Created api-endpoint for container with id " + new_apiendpoint.id + " at " + new_url);
 
     log("Searching for GitLab Environment");
-    var gitlab_env = find_gitlab_environment(gitlab_url, gitlab_token, "review-" + full_sha);
+    var gitlab_env = find_gitlab_environment(gitlab_url, gitlab_token, review_app_name);
     if ( gitlab_env ) {
         log("Calling back to GitLab to provision new environment");
         update_gitlab_environment(gitlab_url, gitlab_token, gitlab_env, {
@@ -108,10 +112,9 @@ function stop(args, ctx) {
     load('https://raw.githubusercontent.com/GalacticFog/lambda-examples/gitlab-demo/js_lambda/gestalt-sdk.js');
     log("***** begin documentation review stop ************\n");
 
-    args = JSON.parse( args );
     ctx  = JSON.parse( ctx );
 
-    META = get_meta(args, ctx.creds);
+    META = get_meta(null, ctx.creds);
     log("[init] found meta: " + META.url);
 
     var tgt_org = find_org(get_env("TARGET_ORG"));
@@ -126,21 +129,20 @@ function stop(args, ctx) {
         return getLog();
     }
 
-    var short_sha = args.substr(0,8);
+    var git_ref    = ctx.params.ref[0];
+    var review_app_name = "review-" + git_ref;
 
-    log("Will delete deployment associated with commit " + short_sha);
+    log("Will delete deployment associated with commit " + git_ref);
 
-    var tgt_cntr = find_container_by_name(tgt_org, tgt_env, "docs-review-" + short_sha);
+    var tgt_cntr = find_container_by_name(tgt_org, tgt_env, "docs-review-" + git_ref);
 
     if ( tgt_cntr ) {
         log("Listing endpoints for container");
         var endpoints = list_container_apiendpoints(tgt_org, tgt_cntr);
         for each (e in endpoints) {
-            log("deleting endpoint " + e.id);
             delete_endpoint(tgt_org, e);
         }
 
-        log("deleting container");
         delete_container(tgt_org, tgt_env, tgt_cntr);
     } else {
         log("did not find any deployments matching that name");
---
+++ js_lambda/example-gitlab-promote.js
@@ -0,0 +1,128 @@
+function promote(args, ctx) {
+    load('https://raw.githubusercontent.com/GalacticFog/lambda-examples/gitlab-demo/js_lambda/gestalt-sdk.js');
+    log("***** begin promote ************");
+
+    args = JSON.parse( args );
+    ctx  = JSON.parse( ctx );
+
+    META = get_meta(args, ctx.creds);
+    log("[init] found meta: " + META.url);
+
+    var tgt_env_id = args.target_env_id;
+    var cur_env_id = args.environment_id;
+
+    var cur_app    = args.resource;
+    var git_ref   = cur_app.properties.labels.GIT_REF;
+
+    var parent_org = find_org(args.fqon);
+
+    var cur_env = find_environment(parent_org, cur_env_id);
+    var tgt_env = find_environment(parent_org, tgt_env_id);
+
+    var gitlab_url   = get_env("GITLAB_API_URL");
+    var gitlab_token = get_env("GITLAB_TOKEN");
+
+    log("request to promote container " + cur_app.name + " from " + cur_env.name + " to environment " + tgt_env.name);
+
+    var tgt_app = find_container_by_name(parent_org, tgt_env, "equity-docs-site");
+    if ( ! tgt_app ) {
+        log("ERROR: could not locate target container in environment " + tgt_env.name + " with name " + cur_app.name);
+        return getLog();
+    }
+
+    log("located target app " + disp(tgt_app) );
+
+    try {
+        patch_container(parent_org, tgt_env, tgt_app, [
+            replace("image", cur_app.properties.image),
+            replace("env",   cur_app.properties.env)
+        ]);
+    } catch(err) {
+        log("ERROR: error creating container: response code " + err);
+        return getLog();
+    }
+    log("Container successfully migrated.");
+
+    log("Listing endpoints for source container");
+    var endpoints = list_container_apiendpoints(parent_org, cur_app);
+    for each (e in endpoints) {
+        delete_endpoint(parent_org, e);
+    }
+    delete_container(parent_org, cur_env, cur_app);
+
+    log("Searching for GitLab Environment");
+    var gitlab_env = find_gitlab_environment(gitlab_url, gitlab_token, "review-" + git_ref);
+    var prod_env = find_gitlab_environment(gitlab_url, gitlab_token, "production");
+    if ( gitlab_env ) {
+        if ( prod_env ) delete_gitlab_environment(gitlab_url, gitlab_token, prod_env);
+
+        log("Calling back to GitLab to update production environment");
+        update_gitlab_environment(gitlab_url, gitlab_token, gitlab_env, {
+            name: "production",
+            external_url: "https://gtw1.demo7.galacticfog.com/equity-docs/"
+        });
+    } else if ( ! prod_env ) {
+        create_gitlab_environment(gitlab_url, gitlab_token, {
+            name: "production",
+            external_url: "https://gtw1.demo7.galacticfog.com/equity-docs/"
+        });
+    } else {
+        log("WARNING: Could not locate GitLab environment in order to update external_url");
+    }
+
+    log("***** end promote **************");
+    return "Container migrated.";
+}
+
+function replace(prop_path, value) {
+    return {
+        op: "replace",
+        path: "/properties/" + prop_path,
+        value: value
+    }
+}
+
+function find_gitlab_environment(base_url, token, environment_name) {
+    log("search for " + environment_name);
+    var url = base_url + "/environments";
+    var pc = client.prepareConnect(url)
+        .setMethod("GET")
+        .addHeader("PRIVATE-TOKEN", token);
+    log("GET " + url, LoggingLevels.DEBUG);
+    var envs = _handleResponse(pc.execute().get());
+    for each (e in envs) if (e.name == environment_name) return e;
+    return null;
+}
+
+function delete_gitlab_environment(base_url, token, gitlab_env) {
+    var url = base_url + "/environments/" + gitlab_env.id;
+    var pc = client.prepareConnect(url)
+        .setMethod("DELETE")
+        .addHeader("PRIVATE-TOKEN", token);
+    log("DELETE " + url, LoggingLevels.DEBUG);
+    return _handleResponse(pc.execute().get());
+}
+
+function update_gitlab_environment(base_url, token, gitlab_env, payload) {
+    var url = base_url + "/environments/" + gitlab_env.id;
+    var pc = client.prepareConnect(url)
+        .setMethod("PUT")
+        .addHeader("PRIVATE-TOKEN", token);
+    log("PUT " + url, LoggingLevels.DEBUG);
+    if (payload) {
+        pc = pc.setBody(JSON.stringify(payload)).addHeader("Content-Type", "application/json")
+    }
+    return _handleResponse(pc.execute().get());
+}
+
+function create_gitlab_environment(base_url, token, payload) {
+    var url = base_url + "/environments";
+    var pc = client.prepareConnect(url)
+        .setMethod("POST")
+        .addHeader("PRIVATE-TOKEN", token);
+    log("POST " + url, LoggingLevels.DEBUG);
+    if (payload) {
+        pc = pc.setBody(JSON.stringify(payload)).addHeader("Content-Type", "application/json")
+    }
+    return _handleResponse(pc.execute().get());
+}
