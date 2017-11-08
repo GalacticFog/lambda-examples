1f78daf116e0490efc729cc993420bc15f1ec415
Brad Futch <brad@galacticfog.com>
2017-11-08 16:55:45 -0500

    a security token generating lambda

---
+++ nodejs_lambda/security_token/index.js
@@ -0,0 +1,64 @@
+var exports = module.exports = {};
+exports.token = function(context, data, callback) {
+
+var protocol = 'http:';
+var querystring = require('querystring');
+
+var http = null;
+if( process.env.SECURITY_PROTOCOL ) {
+    if( process.env.SECURITY_PROTOCOL == 'http' ) {
+        http = require('http');
+    }
+    else {
+        http = require('https');
+        protocol = 'https:';
+    }
+}
+else {
+    http = require('http');
+}
+
+var securityHost = process.env.SECURITY_HOST;
+var securityPort = process.env.SECURITY_PORT;
+var username = process.env.USERNAME;
+var password = process.env.PASSWORD;
+
+console.log( "protocol : " + protocol );
+console.log( "host : " + securityHost );
+console.log( "port : " + securityPort );
+console.log( "user : " + username );
+console.log( "pass : " + password );
+
+var data = querystring.stringify({
+      username: username,
+      password: password,
+			grant_type: "password"
+    });
+
+console.log( "data : " + data );
+
+var options = {
+    protocol: protocol,
+    host: securityHost,
+    port: securityPort,
+    path: '/root/oauth/issue',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)
+    }
+};
+
+var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log("body: " + chunk);
+        callback( chunk );
+    });
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+  });;
+
+req.write(data);
+req.end();
+}
