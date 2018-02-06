b2a5f35bfd0f7be70e9323c4026c118f8f8c75cf
Chris Baker <chris@cgbaker.net>
2018-02-06 16:39:15 -0500

    modified demo setup/teardown lambdas to use latest 1.5 sdk

--- js_lambda/demo-teardown.js
+++ js_lambda/demo-teardown.js
@@ -1,6 +1,6 @@
 function run(/* arguments, credentials */) {
 
-    load('https://raw.githubusercontent.com/GalacticFog/lambda-examples/1.4/js_lambda/gestalt-sdk.js');
+    load('https://raw.githubusercontent.com/GalacticFog/lambda-examples/1.5/js_lambda/gestalt-sdk.js');
 
     META = get_meta(null,null);
     log("found meta: " + META.url, LoggingLevels.DEBUG);
