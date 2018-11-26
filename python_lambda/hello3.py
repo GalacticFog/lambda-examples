a24537f7a5c63f5f83a530e4a0c58e110fb6beec
razzy <rasma.abolina@gmail.com>
2018-11-26 10:51:37 -0500

    Update hello3.py

--- python_lambda/hello3.py
+++ python_lambda/hello3.py
@@ -2,7 +2,9 @@
 
 import sys
 import platform
+print ('--------------------')
 print ('Simple Python Lambda')
 print ('Payload ', str(sys.argv[1]))
 print ('--------------------')
 print ('Python Version', platform.python_version())
+print ('--------------------')
